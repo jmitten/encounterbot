4364ed5030794e562358dcbe1662a45d5f14f91b
Jared <jared@mitten.tech>
2023-07-08 19:25:24 -0400

    project setup

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -3,5 +3,5 @@ data "aws_caller_identity" "current" {}
 data "archive_file" "lambdas" {
   output_path = "lambda.zip"
   type = "zip"
-  source_dir = "${path.module}/../src"
+  source_dir = "${path.module}/src"
 }
