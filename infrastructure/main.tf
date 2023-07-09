763a26e60ee74a87b1280871db4c06fc9260a6c3
Jared <jared@mitten.tech>
2023-07-09 16:45:51 -0400

    Created lambda role

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,3 +17,20 @@ provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name_prefix = "${var.infra_name_prefix}_lambda_role_${var.region}"
+  assume_role_policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Principal = {
+            Service = "lambda.amazonaws.com"
+          },
+          Effect = "Allow"
+        }
+      ]
+    })
+}
