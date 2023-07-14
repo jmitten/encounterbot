b005dae38899a94e6cb41fc8a2c2b2a31fd5e2b5
Jared <jared@mitten.tech>
2023-07-13 22:43:16 -0400

    Finished event support

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -121,25 +121,6 @@ resource "aws_lambda_function" "weekly_query_lambda" {
   }
 }
 
-resource "aws_cloudwatch_event_rule" "birthday_lambda_daily_event_rule" {
-  name = "encounter-bot-birthday-lambda-event-rule"
-  description = "Every day at 12PM UTC"
-  schedule_expression = "cron(0 12 * * ? *)"
-}
-
-resource "aws_cloudwatch_event_target" "birthday_lambda_event_target" {
-  arn = aws_lambda_function.daily_query_lambda.arn
-  rule = aws_cloudwatch_event_rule.birthday_lambda_daily_event_rule.name
-}
-
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_birthday_lambda" {
-  statement_id = "AllowExecutionFromCloudWatch"
-  action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.daily_query_lambda.function_name
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.birthday_lambda_daily_event_rule.arn
-}
-
 
 resource "aws_cloudwatch_event_rule" "lambda_daily_event_rule" {
   name = "encounter-bot-birthday-daily-lambda-event-rule"
